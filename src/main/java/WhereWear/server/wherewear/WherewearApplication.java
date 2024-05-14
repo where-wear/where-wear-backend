c8e83b58871958f04c26fd128a8c6753a5aee8d7
joungeun0825 <joungeun0825@gmail.com>
2024-05-14 11:56:37 +0900

    git init

---
+++ src/main/java/WhereWear/server/wherewear/WherewearApplication.java
@@ -0,0 +1,13 @@
+package WhereWear.server.wherewear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WherewearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WherewearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WhereWear/server/wherewear/WherewearApplicationTests.java
@@ -0,0 +1,13 @@
+package WhereWear.server.wherewear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WherewearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
