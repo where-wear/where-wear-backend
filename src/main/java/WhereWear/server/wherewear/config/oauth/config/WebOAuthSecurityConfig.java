af6a613fd838f057bb53a5c903eb269f2a7af21d
joungeun0825 <joungeun0825@gmail.com>
2024-07-31 14:42:32 +0900

    feat(logout): 로그아웃 구현

--- src/main/java/WhereWear/server/wherewear/config/oauth/config/WebOAuthSecurityConfig.java
+++ src/main/java/WhereWear/server/wherewear/config/oauth/config/WebOAuthSecurityConfig.java
@@ -64,6 +64,13 @@ public class WebOAuthSecurityConfig {
                 .userInfoEndpoint()
                 .userService(oAuth2UserCustomService);
 
+        http.logout()
+                .logoutUrl("/logout")  // 로그아웃 URL 설정
+                .logoutSuccessUrl("/")  // 로그아웃 성공 후 리디렉션할 URL 설정
+                .invalidateHttpSession(true)  // 세션 무효화
+                .deleteCookies("JSESSIONID")  // 쿠키 삭제
+                .permitAll();  // 로그아웃 URL은 모두 접근 가능하게 설정
+
         http.exceptionHandling()
                 .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
                         new AntPathRequestMatcher("/api/**"));
