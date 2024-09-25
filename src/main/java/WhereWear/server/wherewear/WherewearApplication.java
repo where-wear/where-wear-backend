46035613c8e975a310b92d7857dd505fd9b7c031
joungeun0825 <joungeun0825@gmail.com>
2024-09-25 22:28:39 +0900

    fix(log create): 로그 생성 시간 오류 수정

--- src/main/java/WhereWear/server/wherewear/WherewearApplication.java
+++ src/main/java/WhereWear/server/wherewear/WherewearApplication.java
@@ -7,12 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-
+@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class WherewearApplication {
