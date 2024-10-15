9adf42013e8a0c050c1a87bba9ff3f460329b90d
joungeun0825 <joungeun0825@gmail.com>
2024-10-15 10:29:21 +0900

    fix: 버킷 수정

--- src/main/java/WhereWear/server/wherewear/user/account/service/AccountService.java
+++ src/main/java/WhereWear/server/wherewear/user/account/service/AccountService.java
@@ -42,7 +42,7 @@ public class AccountService {
     }
 
     public void setProfileImage(User user, MultipartFile imageFile) throws IOException {
-        ClassPathResource resource = new ClassPathResource("wherewear-437004-4cee393ab731.json");
+        ClassPathResource resource = new ClassPathResource("wherewear-437004-7ad3ba886043.json");
 
         // GoogleCredentials를 사용하여 인증 정보 로드
         InputStream credentialsStream = resource.getInputStream();
