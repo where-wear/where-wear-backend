c8b522fef16296174ae4c195fa01356d606069d6
joungeun0825 <joungeun0825@gmail.com>
2024-11-27 22:39:45 +0900

    fix: 회원가입 경로 수정

--- src/main/java/WhereWear/server/wherewear/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/WhereWear/server/wherewear/config/oauth/OAuth2SuccessHandler.java
@@ -30,7 +30,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
     public static final String HOME_REDIRECT_PATH = "http://wherewear.co.kr/home";
-    public static final String SIGNUP_REDIRECT_PATH = "http://wherewear.co.kr/userdata";
+    public static final String SIGNUP_REDIRECT_PATH = "http://wherewear.co.kr/signin/userdata";
 
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
