d3864eda0bb233bad6bf5c365ef9ebf2edc6ddc9
joungeun0825 <joungeun0825@gmail.com>
2024-11-27 20:05:48 +0900

    fix: 추천 로그 수정

--- src/main/java/WhereWear/server/wherewear/log/recommend/LogRecommendService.java
+++ src/main/java/WhereWear/server/wherewear/log/recommend/LogRecommendService.java
@@ -15,6 +15,21 @@ public class LogRecommendService {
     private final LogRepository logRepository;
 
     public List<Log> getRecommendLogs(String gu, int height, int weight, int footSize, String job) {
+        if(height == 0){
+            return logRepository.findRecommendLogs(gu, 0, 200, weight - 10, weight + 10)
+                    .orElse(Collections.emptyList());
+        }
+
+        if(weight == 0){
+            return logRepository.findRecommendLogs(gu, height - 10, height + 10,0, 200)
+                    .orElse(Collections.emptyList());
+        }
+
+        if(height == 0 && weight == 0){
+            return logRepository.findRecommendLogs(gu, 0, 200,0, 200)
+                    .orElse(Collections.emptyList());
+        }
+
         return logRepository.findRecommendLogs(gu, height - 10, height + 10, weight - 10, weight + 10)
                 .orElse(Collections.emptyList());
     }
