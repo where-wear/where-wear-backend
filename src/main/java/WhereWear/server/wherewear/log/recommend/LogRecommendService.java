a3b951504278cd2d8c18aa90539640bf5345f41e
joungeun0825 <joungeun0825@gmail.com>
2024-11-27 20:43:01 +0900

    fix: 추천 로그 수정

--- src/main/java/WhereWear/server/wherewear/log/recommend/LogRecommendService.java
+++ src/main/java/WhereWear/server/wherewear/log/recommend/LogRecommendService.java
@@ -15,22 +15,15 @@ public class LogRecommendService {
     private final LogRepository logRepository;
 
     public List<Log> getRecommendLogs(String gu, int height, int weight, int footSize, String job) {
-        if(height == 0){
-            return logRepository.findRecommendLogs(gu, 0, 200, weight - 10, weight + 10)
+        if (height == 0 && weight == 0) {
+            return logRepository.findRecommendLogs(gu, 0, 200, 0, 200).orElse(Collections.emptyList());
+        } else if (height == 0) {
+            return logRepository.findRecommendLogs(gu, 0, 200, weight - 10, weight + 10).orElse(Collections.emptyList());
+        } else if (weight == 0) {
+            return logRepository.findRecommendLogs(gu, height - 10, height + 10, 0, 200).orElse(Collections.emptyList());
+        } else {
+            return logRepository.findRecommendLogs(gu, height - 10, height + 10, weight - 10, weight + 10)
                     .orElse(Collections.emptyList());
         }
-
-        if(weight == 0){
-            return logRepository.findRecommendLogs(gu, height - 10, height + 10,0, 200)
-                    .orElse(Collections.emptyList());
-        }
-
-        if(height == 0 && weight == 0){
-            return logRepository.findRecommendLogs(gu, 0, 200,0, 200)
-                    .orElse(Collections.emptyList());
-        }
-
-        return logRepository.findRecommendLogs(gu, height - 10, height + 10, weight - 10, weight + 10)
-                .orElse(Collections.emptyList());
     }
 }
