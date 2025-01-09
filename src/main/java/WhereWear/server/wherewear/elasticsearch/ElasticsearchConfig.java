30f6ae35a09fec97295031fc79d5920da8ce6003
joungeun0825 <joungeun0825@gmail.com>
2025-01-10 00:41:14 +0900

    fix: url 주소 수정

--- src/main/java/WhereWear/server/wherewear/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/WhereWear/server/wherewear/elasticsearch/ElasticsearchConfig.java
@@ -1,21 +1,20 @@
 package WhereWear.server.wherewear.elasticsearch;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
 @Configuration
-public class ElasticsearchConfig {
-    @Primary
-    @Bean
-    public RestHighLevelClient restHighLevelClient() {
-        return new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http") // Elasticsearch 호스트와 포트 설정
-                )
-        );
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String[] esHost;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(esHost)
+                .build();
     }
 }
